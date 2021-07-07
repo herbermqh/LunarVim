9079e2b098df36857da573e76662dc13538594fc
Jim Cornmell <jim.cornmell@costcutter.com>
2021-07-07 11:58:43 +0100

    Dial setup

--- lua/lv-dial/init.lua
+++ lua/lv-dial/init.lua
@@ -12,6 +12,22 @@ vmap g<C-x> <Plug>(dial-decrement-additional)
 
   local dial = require "dial"
 
+  dial.config.searchlist.normal = {
+    "number#decimal#fixed#zero",
+    "number#hex",
+    "number#binary",
+    "date#[%Y/%m/%d]",
+    "date#[%Y-%m-%d]",
+    "date#[%H:%M:%S]",
+    "date#[%H:%M]",
+    "char#alph#small#word",
+    "char#alph#capital#word",
+    "char#alph#small#str",
+    "char#alph#capital#str",
+    "color#hex",
+    "markup#markdown#header",
+  }
+
   dial.augends["custom#boolean"] = dial.common.enum_cyclic {
     name = "boolean",
     strlist = { "true", "false" },
@@ -24,6 +40,15 @@ vmap g<C-x> <Plug>(dial-decrement-additional)
     strlist = { "True", "False" },
   }
   table.insert(dial.config.searchlist.normal, "custom#Boolean")
+
+  -- For Calendar.
+  dial.augends["custom#Calendar"] = dial.common.enum_cyclic {
+    name = "Calendar",
+    strlist = {
+      "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    },
+  }
+  table.insert(dial.config.searchlist.normal, "custom#Calendar")
 
 end
 
