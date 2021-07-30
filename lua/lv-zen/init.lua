aa1a6d5a762bb08912caae639a58c6e37eedd963
Jim Cornmell <jim.cornmell@costcutter.com>
2021-07-30 14:37:14 +0100

    Zen mode and a tidy

--- lua/lv-zen/init.lua
+++ lua/lv-zen/init.lua
@@ -1,10 +1,30 @@
 local M = {}
-local status_ok, zen_mode = pcall(require, "zen-mode")
-if not status_ok then
-  return
-end
 
 M.config = function()
-  zen_mode.setup(O.plugin.zen)
+    require("zen-mode").setup({
+        window = {
+            backdrop = 1,
+            height = 1,                     -- height of the Zen window
+            width = 1,                      -- height of the Zen window
+            options = {
+                signcolumn = "no",          -- disable signcolumn
+                number = true,              -- disable number column
+                relativenumber = false,     -- disable relative numbers
+                cursorline = true,          -- disable cursorline
+                cursorcolumn = false,       -- disable cursor column
+                foldcolumn = "0",           -- disable fold column
+                list = false,               -- disable whitespace characters
+            },
+        },
+
+        plugins = {
+            gitsigns = { enabled = false }, -- disables git signs
+            kitty = {
+                enabled = true,
+                font = "+5",                -- font size increment
+            }
+        },
+    })
 end
+
 return M
---
+++ media/visual_appearance.vim
@@ -0,0 +1,23 @@
+" Highlight URL's. http://www.vivaldi.com
+
+" Highlight todo comments.
+"  PERF:Something to describe.
+"  HACK:Something to describe.
+"  TODO:Something to describe.
+"  NOTE:Something to describe.
+"  WARNING:Something to describe.
+"  FIX:Something to describe.
+"  BUG:Something to describe.
+"  FIXME: Something to describe.
+
+" Highlight typo's in spellingg.
+
+" Highlight issues in red:
+" highlightissuesinred
+
+" Highlight colours:
+" Name   : Red Orange Pink Yellow Green Blue White Black
+" RGB    : #F00 #0F0 #00F #0FF #F0F #FF0
+# RRGGBB : #FF0000 #608b4e #d16969  #343434 #605555
+
+
